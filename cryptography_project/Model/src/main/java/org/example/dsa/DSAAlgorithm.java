2853e12eeb94035b6cbfe307ca5ee06f5735f6a2
wzyndul <wojteckiz8630@gmail.com>
2023-04-25 21:32:59 +0200

    added DSAAlgorithm class with specyfication for the task

---
+++ cryptography_project/Model/src/main/java/org/example/dsa/DSAAlgorithm.java
@@ -0,0 +1,17 @@
+package org.example.dsa;
+
+public class DSAAlgorithm {
+
+
+    // generowanie klucza
+
+    //generuje sie od razu 4 klucze ? q oraz g klucz publiczny y prywatny x MOD p
+
+    // podpisywanie byte array - pliki etc
+    // podipsywanie tekstu ?? tak ma rogowski
+    // odpowiednio deszyfrowanie tego i tego
+
+
+    // trzebaz BIG inigera korzystac
+    // czy trzeba robić skrót wiadomosci (hash) >???? i dopiero go szyfrowac
+}
